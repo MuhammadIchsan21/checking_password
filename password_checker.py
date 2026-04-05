6b183da33ef67220ff1cd0c937ef7eadeb10e501
MuhammadIchsan21 <39551278+MuhammadIchsan21@users.noreply.github.com>
2026-04-05 08:28:51 +0800

    password_checker 8  karakter

---
+++ password_checker.py
@@ -0,0 +1,64 @@
+# password_checker.py
+# Project: Password Strength Checker
+# Day 1 - Cybersecurity Learning
+
+def check_password_strength(password):
+    """
+    Function untuk cek kekuatan password
+    Input: password (string)
+    Output: score dan feedback
+    """
+    score = 0 
+    feedback = []
+
+    #Kita akan tes logic disini
+
+    # return score, feedback
+
+# ===== CEK 1: PANJANG PASSWORD =====
+# len() function menghitung jumlah karakter
+    if len(password) >= 8:
+        score += 1
+        feedback.append("✅ Panjang cukup (8+ karakter)")
+    else:
+        feedback.append("❌ Terlalu pendek! Minimal 8 karakter")
+
+# # ===== CEK 2: HURUF BESAR =====
+#     if any(c.isupper() for c in password):
+#         score += 1
+#         feedback.append("✅ Mengandung huruf besar")
+#     else:
+#         feedback.append("❌ Tambahkan huruf besar")
+
+# # ===== CEK 3: HURUF KECIL =====
+#     if any(c.islower() for c in password):
+#         score += 1
+#         feedback.append("✅ Mengandung huruf kecil")
+#     else:
+#         feedback.append("❌ Tambahkan huruf kecil")
+
+# # ===== CEK 4: ANGKA =====
+#     if any(c.isdigit() for c in password):
+#         score += 1
+#         feedback.append("✅ Mengandung angka")
+#     else:
+#         feedback.append("❌ Tambahkan angka")
+
+# # ===== CEK 5: SIMBOL =====
+#     if any(c in "!@#$%^&*()" for c in password):
+#         score += 1
+#         feedback.append("✅ Mengandung simbol")
+#     else:
+#         feedback.append("❌ Tambahkan simbol (!@#$%^&*)")
+
+    return score, feedback
+
+# Test dengan berbagai password
+test_passwords = ["abc", "test1234", "verylongpassword"]
+for pwd in test_passwords:
+    score, feedback = check_password_strength(pwd)
+    print(f"\nPassword: {pwd}")
+    print(f"Score: {score}/1")
+    for f in feedback:
+        print(f)
+    
